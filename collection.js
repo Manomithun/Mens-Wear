9715a8364c8b8925ef589a08de657a173b4f2920
Manomithun <162666856+Manomithun@users.noreply.github.com>
2024-10-20 15:39:54 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,15 @@
+var productcontainer=document.querySelector(".collectionnext");
+var search=document.getElementById("search");
+var porductlist=productcontainer.querySelectorAll("div")
+search.addEventListener("keyup",function(){
+    var entervalue=event.target.value.toUpperCase();
+    for(count=0;count<porductlist.length;count=count+1){
+        var name=porductlist[count].querySelector("p").textContent;
+        if(name.toUpperCase().indexOf(entervalue)<0){
+            porductlist[count].style.display="none";
+        }
+        else{
+            porductlist[count].style.display="block";
+        }
+    }
+})
---
+++ j.js
@@ -0,0 +1,7 @@
+var navlink1=document.querySelector(".navlink1");
+function opennav(){
+    navlink1.style.left="0";
+}
+function closenav(){
+    navlink1.style.left="-60%";
+}
